5fc86f18112f356514b3bdddd11ce1d16a20ac9b
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-10-13 14:15:46 -0300

    Update HRApp.java

--- src/HRApp.java
+++ src/HRApp.java
@@ -1,12 +1,24 @@
 public class HRApp {
 
     public static void main(String[] args) {
-        Employee employee1 = new Employee(1, "Thiago", 3.500);
-        Employee employee2 = new Employee(2, "Janaina", 2.500);
-        Employee employee3 = new Employee(3, "Luzia", 4.500);
-        Employee employee4 = new Employee(4, "Suelani", 2.000);
+        Employee employee1 = new Employee(1, "Thiago", 3500.0);
+        Employee employee2 = new Employee(2, "Janaina", 2500.0);
+        Employee employee3 = new Employee(3, "Luzia", 4500.0);
+        Employee employee4 = new Employee(4, "Suelani", 2000.0);
+
+        Department dep1 = new Department("Sala");
+
+        dep1.addEmployee(employee1);
+        dep1.addEmployee(employee2);
+        dep1.addEmployee(employee3);
+        dep1.addEmployee(employee4);
+
+        Employee[] employees = dep1.getEmployees();
+
+        for(Employee emp : employees){
+            System.out.println("Employees " + emp);
+        }
 
         System.out.println(employee1);
-        System.out.println();
     }
 }
