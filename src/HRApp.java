db8b326de0798ab9ddfbee787c69400b736f18d7
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-10-13 13:57:17 -0300

    Update HRApp.java

--- src/HRApp.java
+++ src/HRApp.java
@@ -1,8 +1,12 @@
 public class HRApp {
 
     public static void main(String[] args) {
-        Employee employee = new Employee(1, "Thiago", 3.500);
+        Employee employee1 = new Employee(1, "Thiago", 3.500);
+        Employee employee2 = new Employee(2, "Janaina", 2.500);
+        Employee employee3 = new Employee(3, "Luzia", 4.500);
+        Employee employee4 = new Employee(4, "Suelani", 2.000);
 
-        System.out.println(employee);
+        System.out.println(employee1);
+        System.out.println();
     }
 }
